060b8f59207437b5fa946c401089b0d314f15bf1
Kevin Cabezas <kevin.23cabezas@gmail.com>
2022-10-27 22:24:42 -0500

    Security groups were separated

---
+++ common-environment/sg.tf
@@ -0,0 +1,63 @@
+# Security groups
+resource "aws_security_group" "concurso_public_sg" {
+  name        = "concurso-public-sg"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.vpc_concurso.id
+
+  ingress {
+    description      = "Access SSH from Internet"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "Access HTTP from Internet"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "concurso-public-sg"
+  }
+}
+
+resource "aws_security_group" "concurso_private_sg" {
+  name        = "concurso-private-sg"
+  description = "Allow SSH outbound traffic"
+  vpc_id      = aws_vpc.vpc_concurso.id
+
+  ingress {
+    description      = "Access SSH from Internet"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [var.public_subnet_cird]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "concurso-private-sg"
+  }
+}
