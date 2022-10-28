890d632e84a25b6c6b028e492e09bef2a1b78034
Kevin Cabezas <kevin.23cabezas@gmail.com>
2022-10-27 22:15:26 -0500

    Private EC2 instance was created

--- common-environment/ec2.tf
+++ common-environment/ec2.tf
@@ -1,7 +1,7 @@
 # Creates EC2 instances
 
-resource "aws_instance" "web" {
-  count                  = 2
+resource "aws_instance" "public-nginx" {
+  count                  = 1
   ami                    = var.instance_ami
   instance_type          = var.instance_type
   key_name               = "kevinLabs"
@@ -9,6 +9,23 @@ resource "aws_instance" "web" {
   vpc_security_group_ids = [aws_security_group.concurso_public_sg.id]
   tags = {
     Name = "public-concurso"
+  }
+  lifecycle {
+    ignore_changes = [
+      ebs_block_device
+    ]
+  }
+}
+
+resource "aws_instance" "private-mysql" {
+  count                  = 1
+  ami                    = var.instance_ami
+  instance_type          = var.instance_type
+  key_name               = "kevinLabs"
+  subnet_id              = aws_subnet.private_subnet_concurso.id
+  vpc_security_group_ids = [aws_security_group.concurso_private_sg.id]
+  tags = {
+    Name = "private-concurso"
   }
   lifecycle {
     ignore_changes = [
