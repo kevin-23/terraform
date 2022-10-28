c6c83af1ec352d1b76ae4d77efa7a9a4e2caef18
Kevin Cabezas <kevin.23cabezas@gmail.com>
2022-10-28 00:16:55 -0500

    resources were renamed

--- common-environment/ec2.tf
+++ common-environment/ec2.tf
@@ -1,6 +1,6 @@
 # Creates EC2 instances
 
-resource "aws_instance" "public-nginx" {
+resource "aws_instance" "bastion_concurso" {
   count                  = 1
   ami                    = var.instance_ami
   instance_type          = var.instance_type
@@ -8,7 +8,7 @@ resource "aws_instance" "public-nginx" {
   subnet_id              = aws_subnet.public_subnet_concurso.id
   vpc_security_group_ids = [aws_security_group.concurso_public_sg.id]
   tags = {
-    Name = "public-concurso"
+    Name = "bastion-concurso"
   }
   lifecycle {
     ignore_changes = [
@@ -17,15 +17,15 @@ resource "aws_instance" "public-nginx" {
   }
 }
 
-resource "aws_instance" "private-mysql" {
-  count                  = 1
+resource "aws_instance" "private_nginx_concurso" {
+  #count                  = 1
   ami                    = var.instance_ami
   instance_type          = var.instance_type
   key_name               = "kevinLabs"
   subnet_id              = aws_subnet.private_subnet_concurso.id
   vpc_security_group_ids = [aws_security_group.concurso_private_sg.id]
   tags = {
-    Name = "private-concurso"
+    Name = "private-nginx-concurso"
   }
   lifecycle {
     ignore_changes = [
