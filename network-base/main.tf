661e4b657d1965ef4583585b423c04333d7fed8c
Kevin Cabezas <kevin.23cabezas@gmail.com>
2023-08-11 09:47:24 -0500

    the provider was separated

--- network-base/main.tf
+++ network-base/main.tf
@@ -1,17 +1,3 @@
-# Configure the AWS Provider
-# Keys are environment variables
-provider "aws" {
-  region = "us-east-1"
-
-  default_tags {
-    tags = {
-      environment = "test"
-      owner       = "kevin.cabezas"
-      tool        = "terraform"
-    }
-  }
-}
-
 module "vpc" {
   source = "./vpc"
 }
